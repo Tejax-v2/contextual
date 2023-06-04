71257742cb12c40100edfa3006bae2219078853a
Tsung-Han Yu <14802181+johan456789@users.noreply.github.com>
2023-06-04 15:25:13 -0400

    mvp

---
+++ app.py
@@ -0,0 +1,18 @@
+import string
+import streamlit as st
+
+from llm import define
+
+
+st.title('Contextual Dictionary')
+st.text('Type in a word and the sentence where it\'s used and get the definition.\nPowered by Claude API.')
+
+sentence = st.text_area('Sentence', 'This is a word in an example sentence.')
+
+stripped_sent = sentence.translate(str.maketrans('', '', string.punctuation))
+words = stripped_sent.split(' ')
+word = st.selectbox('Word to Define', [''] + list(words), key='word')
+
+st.write('---')
+if word:
+    st.write(f'The word "{st.session_state.word}" in the sentence means:\n\n{define(word, sentence)}') # type: ignore
